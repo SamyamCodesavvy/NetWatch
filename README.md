2f2d83092f6eac711e13f939a9468234548a69fa
gireesamyam <samyam0913@gmail.com>
2026-07-02 11:44:05 +0545

    cleanup

--- README.md
+++
@@ -1,9 +0,0 @@
-# NetWatch
-
-**Network Monitoring and Infrastructure Management Platform**
-
-## System Overview
-
-<p align="center">
-  <img src="architecture/netwatch-arch-2.png" alt="NetWatch System Architecture" width="900">
-</p>
--- app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.models.user import User, UserRole
-from app.models.device import Device, Environment
-from app.models.monitoring import (
-    PingResult, PortCheckResult, HttpCheckResult,
-    DnsCheckResult, SslCheckResult, SshMetricResult,
-)
-
-from app.models.alert import Alert, AlertSeverity
